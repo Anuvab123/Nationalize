5291eba3d1f35523216299b1fd07873c7dbc0573
Anuvab123 <ghoshanuvab2000@gmail.com>
2023-04-13 23:48:27 +0530

    Initial commit

---
+++ Nationalize/src/test/java/com/geekster/Nationalize/NationalizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekster.Nationalize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NationalizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
